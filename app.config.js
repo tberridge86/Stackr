50a6482264407347897c0677803c2db4399807df
Tom Berridge <tberridge86@gmail.com>
2026-05-22 23:23:52 +0100

    ios build changes

--- app.config.js
+++ app.config.js
@@ -1,20 +1,20 @@
-const base = require('./app.json');
-
 const isDevApp = process.env.APP_VARIANT === 'development';
 
-module.exports = {
-  expo: {
-    ...base.expo,
-    name: isDevApp ? 'Stackr Dev' : base.expo.name,
-    slug: isDevApp ? 'Stackr-dev' : base.expo.slug,
-    scheme: isDevApp ? 'StackrDev' : base.expo.scheme,
-    ios: {
-      ...base.expo.ios,
-      bundleIdentifier: isDevApp ? 'com.tommo86.Stackr.dev' : base.expo.ios.bundleIdentifier,
-    },
-    android: {
-      ...base.expo.android,
-      package: isDevApp ? 'com.tommo86.Stackr.dev' : base.expo.android.package,
-    },
+module.exports = ({ config }) => ({
+  ...config,
+  name: isDevApp ? 'Stackr Dev' : config.name,
+  slug: isDevApp ? 'stackr-dev' : config.slug,
+  scheme: isDevApp ? 'stackr-dev' : config.scheme,
+  ios: {
+    ...config.ios,
+    bundleIdentifier: isDevApp
+      ? 'com.tommo86.Stackr.dev'
+      : config.ios.bundleIdentifier,
   },
-};
+  android: {
+    ...config.android,
+    package: isDevApp
+      ? 'com.tommo86.Stackr.dev'
+      : config.android.package,
+  },
+});
